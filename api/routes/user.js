3bb8684610b39179ee92b686e582632d26ba1e9b
Ammar Yasser <Ammarasc74@gmail.com>
2021-03-24 06:20:38 +0200

    add get all user controller

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,12 +5,13 @@ const router = express.Router();
 
 // const checkAuth = require('../middleware/check-auth');
 const User = require('../models/user');
-const UserController = require('../controllers/users/create_user');
+const createUserController = require('../controllers/users/create_user');
+const getAllUserController = require('../controllers/users/users');
 
 
-// router.get("/users", UserController.get_all_users);
+router.get("/users", getAllUserController.get_all_users);
 
-router.post("/signup", UserController.user_signup);
+router.post("/signup", createUserController.user_signup);
 
 
 module.exports = router;
